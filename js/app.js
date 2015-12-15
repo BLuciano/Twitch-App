0037d9af95edf9f9eb5727760b02e5da1955b081
Luciano Bruzzoni <lucianobruzzoni@yahoo.com>
2015-12-14 21:15:59 -0800

    API data is now displayed

--- js/app.js
+++ js/app.js
@@ -4,39 +4,44 @@
 	$("button").on("click", function(){
 		$("button").removeClass("selected");
 		$(this).addClass("selected");
+		console.log($(this).html());
+		//if($(this).html() === "All" 
 	});
 
 	var streamers = [
 		"freecodecamp", "storbeck", "terakilobyte", 
 		"habathcx", "RobotCaleb", "thomasballinger",
-		"noobs2ninjas","beohoff"
+		"noobs2ninjas","beohoff", "medrybw"
 	];
 	
 	for(var i = 0; i < streamers.length; i++){
 		/*Get channel information for each streamer.*/
 		url = "https://api.twitch.tv/kraken/channels/" + streamers[i] + "?callback=?";
-		$.getJSON(url).done(function(data){
-			content = "";
-			content+= "<a href='" + data.url + "'>";
-			content+= "<img class='left' src='" + data.logo + "'>";
-			content+= "<h4 class='left'>" + data.display_name + "</h4>";
-			console.log(content);
+		$.getJSON(url)
+		.done(function(data){
+			var info = data; 
+			
 			/*Check to see if streamer is online or offline. Done by different call.
 			add different data according to online status.*/
-			$.getJSON("https://api.twitch.tv/kraken/streams/" + streamers[i] + "?callback=?").
-			done(function(data){
+			$.getJSON("https://api.twitch.tv/kraken/streams/" + data.display_name + "?callback=?",
+			function(data){
+				content = "";
+				content+= "<a target='_blank' href='" + info.url + "'>";
+				content+= "<img class='left' src='" + info.logo + "'>";
+				content+= "<h4 class='left'>" + info.display_name + "</h4>";
+
 				if(data.stream){
 					content+= "<h4 class='right online'><i class='fa fa-thumbs-up'></i></h4>";
 					content+= "<p>" + data.stream.channel.status + "</p></a>";
-					online+= content; 		
+					online+= content;		
 				} else {
 					content+= "<h4 class='right offline'><i class='fa fa-thumbs-down'></i></h4>";		
-					content+="<p></p></a>";
+					content+="<p>&nbsp</p></a>";
 					offline+= content;
 				}
-			}); 
+				all+= content;
+				$("#display").append(content);
+			});
 		});
-		all+= content;
-		console.log(content);
 	}
 })();
