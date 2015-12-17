2e50c6fa947c98a228d0412135946ce567854d01
Luciano Bruzzoni <lucianobruzzoni@yahoo.com>
2015-12-16 21:30:17 -0800

    Redesigned data storage to allow for searching

--- js/app.js
+++ js/app.js
@@ -1,6 +1,6 @@
 (function(){
-	var url, content, all ="", online ="", offline ="";
-	var image;
+	var url, content, all =[], online =[], offline =[];
+	var image, value;
 	var streamers = [
 		"freecodecamp", "storbeck", "terakilobyte", 
 		"habathcx", "RobotCaleb", "thomasballinger",
@@ -13,15 +13,24 @@
 		$(this).addClass("selected");
 
 		if($(this).html() === "All"){
-			$("#display").html(all);
+			display(all)
 		} 
 		if($(this).html() === "Online"){
-			$("#display").html(online);
+			display(online);
 		}
 		if($(this).html() === "Offline"){
-			$("#display").html(offline);
+			display(offline);
 		}
 	});
+
+	//Displays streamers according to button selection.
+	function display(array){
+		$("#display").html("");
+		var keys = Object.keys(array);
+		for(var i = 0; i < keys.length; i++){
+			$("#display").append(array[keys[i]]);
+		}
+	}
 
 	//Json API calls section
 	for(var i = 0; i < streamers.length; i++){
@@ -56,20 +65,34 @@
 					if(data.stream){
 						content+= "<h4 class='right online'><i class='fa fa-thumbs-up'></i></h4>";
 						content+= "<p>" + data.stream.channel.status + "</p></a>";
-						online+= content;		
+						online[info.display_name.toLowerCase()] = content;		
 					} else {
 						content+= "<h4 class='right offline'><i class='fa fa-thumbs-down'></i></h4>";		
 						content+="<p></p></a>";
-						offline+= content;
+						offline[info.display_name.toLowerCase()] = content;
 					}
 				}
-				all+= content;
+				all[info.display_name.toLowerCase()] = content;
 				$("#display").append(content);
 			});
 		});
 	}
 
 	//Search bar section
+	$("#search-bar").on('keyup paste', function(){
+    	value = $(this).val().toLowerCase();
+    	if(value === ""){
+    		display(all);
+    	}
+    	else{
+    		$("#display").html(""); 
+    		index = Object.keys(all).indexOf(value);
+    		if(index !== -1){	
+    			console.log(all[Object.keys(all)[index]]);
+    			$("#display").append(all[Object.keys(all)[index]]);
+    		}
+    	}
+    });
 
 
 })();
